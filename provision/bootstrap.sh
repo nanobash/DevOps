4065bebd036bb888c14655f95819ede26f54e567
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2017-03-10 23:05:12 +0200

    Refactors GPG key adding into one line

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -9,10 +9,7 @@ echo "deb http://packages.dotdeb.org jessie all" >> /etc/apt/sources.list;
 echo "deb-src http://packages.dotdeb.org jessie all" >> /etc/apt/sources.list;
 
 # Fetch and install the Dotdeb repository GnuPG key
-cd /tmp;
-wget https://www.dotdeb.org/dotdeb.gpg;
-sudo apt-key add dotdeb.gpg;
-rm dotdeb.gpg;
+wget -qO- https://www.dotdeb.org/dotdeb.gpg | sudo apt-key add -;
 # **************************************************************************************
 
 # ********************  Basic Operations  **********************************************
