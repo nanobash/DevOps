e57e9b011ecbabc8dfacabcfbfe26e37b8c4b6a4
Tornike Nanobashvili <tnanobashvili92@gmail.com>
2017-03-10 19:54:23 +0200

    Updates bootstrap.sh
    	- Adds dotdeb repository and its GPG key
    	- Installs nginx web server
    	- Installs php7.0 and some of the packages
    	- Installs htop pcregrep siege utilities
    	- Configures php7.0 with provisioned .conf files
    	- Configures nginx web server and enables html.conf virtual host
    	- Restarts php and nginx services

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -4,15 +4,55 @@
 # *        Maintains debian/jessie64 Operating System        *
 # ************************************************************
 
-# ********************  Basic Operations  ********************
+# ********************  Adds Dotdeb repository  ****************************************
+echo "deb http://packages.dotdeb.org jessie all" >> /etc/apt/sources.list;
+echo "deb-src http://packages.dotdeb.org jessie all" >> /etc/apt/sources.list;
+
+# Fetch and install the Dotdeb repository GnuPG key
+cd /tmp;
+wget https://www.dotdeb.org/dotdeb.gpg;
+sudo apt-key add dotdeb.gpg;
+rm dotdeb.gpg;
+# **************************************************************************************
+
+# ********************  Basic Operations  **********************************************
 # Fixes broken dependencies
-sudo apt-get -f install
+sudo apt-get -f install;
 # Removes unnecessary packages
-sudo apt-get -y autoremove
+sudo apt-get -y autoremove;
 # Cleans repository cache
-sudo apt-get clean all
+sudo apt-get clean all;
 # Update packages
-sudo apt-get -y update
+sudo apt-get -y update;
 # Upgrade packages
-sudo apt-get -y upgrade
-# ************************************************************
+sudo apt-get -y upgrade;
+# **************************************************************************************
+
+# Installs nginx web server
+sudo apt-get -y install nginx;
+
+# Installs php7.0 and packages
+sudo apt-get -y install php7.0 php7.0-fpm php7.0-cli \
+                        php7.0-curl php7.0-gd php7.0-json php7.0-mcrypt php7.0-mbstring;
+
+# Installs handy utilities
+sudo apt-get -y install htop pcregrep siege;
+# **************************************************************************************
+
+# Configures php7.0
+sudo rm -f "/etc/php/7.0/fpm/pool.d/www.conf";
+sudo mv "/tmp/php-fpm.conf" "/etc/php/7.0/fpm/php-fpm.conf";
+sudo mv "/tmp/php-pool-html.conf" "/etc/php/7.0/fpm/pool.d/html.conf";
+
+# Configures nginx web server
+sudo rm -f "/etc/nginx/sites-available/default";
+sudo rm -f "/etc/nginx/sites-enabled/default";
+sudo mv "/tmp/nginx-html.conf" "/etc/nginx/sites-available/html.conf";
+cd "/etc/nginx/sites-enabled";
+sudo ln -s "../sites-available/html.conf";
+# **************************************************************************************
+
+# Restart services
+sudo systemctl restart php7.0-fpm.service;
+sudo systemctl restart nginx.service;
+# **************************************************************************************
